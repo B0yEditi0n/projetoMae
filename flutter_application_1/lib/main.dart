26473b069d7124e719a3d9d2a7b5ef5f2046a47b
B0yEditi0n <ccaioabreudesouza9874@gmail.com>
2022-05-28 16:23:48 -0300

    adicionado firebase no projeto

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/funcionalidades/bloqueioAparelho.dart';
 import 'package:flutter_application_1/funcionalidades/detalhes_usuario.dart';
